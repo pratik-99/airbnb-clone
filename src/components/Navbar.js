d759f8136c2ee1f2ea1b8cb7ad95ebdeaabbc3fb
pratikpoojary <pratik.poojary@somaiya.edu>
2023-08-21 20:23:23 +0530

    airbnb commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../images/airbnb-logo.svg" className="nav--logo" />
+        </nav>
+    )
+}
